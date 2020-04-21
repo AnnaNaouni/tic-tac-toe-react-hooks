e8b1aeb521252a63f088f02b712e44fb7161a1af
AnnaNaouni <41639045+AnnaNaouni@users.noreply.github.com>
2020-04-21 14:06:13 +0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-﻿import React from 'react';
-import ReactDOM from 'react-dom';
-import Game from "./components/Game.js";
-import './index.css';
-
-ReactDOM.render(<Game />, document.getElementById("root"));
